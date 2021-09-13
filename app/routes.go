5fc44fdf96e39b5e1303b1f6674efbf2a27d4c32
Adiputra <tuts.adiputra@gmail.com>
2021-09-13 08:22:22 +0700

    tenplate unrolled/render

--- app/routes.go
+++ app/routes.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/adiputra22/golangtoko/app/controllers"
 	"github.com/gorilla/mux"
 )
@@ -9,4 +11,9 @@ func (server *Server) initializeRoutes() {
 	server.Router = mux.NewRouter()
 
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
+
+	// static file handler
+	staticFileDirectory := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/public/", http.FileServer(staticFileDirectory))
+	server.Router.PathPrefix("/public/").Handler(staticFileHandler).Methods("GET")
 }
